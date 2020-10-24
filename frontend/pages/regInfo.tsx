a62d0d4ee9ce1d1a49526ae18a9f99b84bc91378
alansyue <b123105@gmail.com>
2020-10-24 18:38:17 +0800

    feat: add reg info rule block

--- frontend/pages/regInfo.tsx
+++ frontend/pages/regInfo.tsx
@@ -70,6 +70,45 @@ const ContentBlockCenter = styled(ContentBlock)`
     text-align: center;
 `;
 
+const RegInfoDetailDiv = styled.div`
+    width: 750px;
+    height: 1000px;
+    margin-top: 60px;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: 10%;
+    margin-left: 15%;
+    text-align: center;
+    padding: 0;
+    font-family: 'Noto Sans TC',Roboto;
+    line-height: 2;
+
+    .title-block {
+        text-align: center;
+        margin: 15px 0;
+        width: 33.33333333%;
+        float: left;
+    }
+
+    .title-block span {
+        padding: 5px 10px;
+        border: 1px solid #145488;
+        font-size: 16pt;
+        color: #145488;
+        margin: 0;
+        font-family: 'Noto Sans TC',Roboto;
+        line-height: 2;
+    }
+
+    p {
+        text-align: left;
+        margin: 15px 0;
+        font-size: 16pt;
+        width: 66.66666667%;
+        float: left;
+    }
+`;
+
 const RegInfo = () => {
     return (
         <>
@@ -98,6 +137,34 @@ const RegInfo = () => {
                 <ContentBlockCenter>
                     <h2>「勇敢成就平凡的自己！ 」</h2>
                 </ContentBlockCenter>
+                <RegInfoDetailDiv>
+                    
+                        <div className="title-block">
+                            <span>- 培訓時間 -</span>
+                        </div>
+                        <p>2020 年 9 月 ~ 2021 年 8 月</p>
+                        <div className="title-block">
+                            <span>- 培訓地點 -</span>
+                        </div>
+                        <p>新北市電腦商業同業公會</p>
+                        <div className="title-block">
+                            <span>- 培訓費用 -</span>
+                        </div>
+                        <p>一年 2000 元</p>
+                        <div className="title-block">
+                            <span>- 報名資格 -</span>
+                        </div>
+                        <p>升大二至升碩一之全台大專院校學生，不限科系</p>
+                        <div className="title-block">
+                            <span>- 報名時間 -</span>
+                        </div>
+                        <p>2020/06/06（六）00:00   至   2020/07/17（五）23:59</p>
+                        <div className="title-block">
+                            <span>- 甄選流程 -</span>
+                        </div>
+                        <p>資料初審 → 公布面試名單（7/24） → 團體面試（8/2） → 公布錄取（8/8）</p>
+                    
+                </RegInfoDetailDiv>
             </RegInfoDiv>
         <TemplateFooter />
         </>
