767bfe28cc391fb4d50fa3a3450710d8c9b5b24c
薛翔文 <alansyue@xiesonghuade-MacBook-Pro-2.local>
2020-11-06 18:28:45 +0800

    feat: set is_verify colmun default false

--- backend/src/migration/1604658399097-changeIsVerifyDefault.ts
+++ backend/src/migration/1604658399097-changeIsVerifyDefault.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class changeIsVerifyDefault1604563139969 implements MigrationInterface {
-    name = 'changeIsVerifyDefault1604563139969'
+export class changeIsVerifyDefault1604658399097 implements MigrationInterface {
+    name = 'changeIsVerifyDefault1604658399097'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "is_verify" SET DEFAULT false`);
