a66201c98031d283c4d9a5066892cb8ce6ed2260
alansyue <alan.syue@kuobrothers.com>
2021-02-09 11:50:56 +0800

    feat: api route add prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api');
   const options = new DocumentBuilder()
     .setTitle('ItSeed')
     .setDescription('The ItSeed API description')
